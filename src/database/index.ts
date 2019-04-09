6cb1071d493f010e4717d20e0f73afc64da794bd
Zabcode <hunter.eal@yandex.ru>
2019-04-09 21:43:27 +0300

    initial

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,12 +2,12 @@ import 'reflect-metadata';
 import { Sequelize } from 'sequelize-typescript';
 
 export const sequelize = new Sequelize({
-  database: process.env.POSTGRES_URI || 'boilerplate',
+  database: 'boilerplate',
   dialect: 'postgres',
   username: 'postgres',
   password: '123123',
   storage: ':memory:',
-  host: '192.168.99.100',
-  port: 6543,
+  host: process.env.DB_HOST || '192.168.99.100',
+  port: 5432,
   modelPaths: [__dirname + '../api/**/*.model.ts']
 });
